235ab960faf1be3141bc9ff8a5b311a08947fc73
Kajtek2566 <kajetan.stach@microsoft.wsei.edu.pl>
2023-11-06 22:48:23 +0100

    11/6 v2.4

--- Lab  1 ASP.NET/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab__1_ASP.NET.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
